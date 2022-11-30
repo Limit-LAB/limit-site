dd539aad73107ec593d5d9c761dd4041aaa936b9
George Miao <gm@miao.dev>
2022-11-30 16:11:09 -0500

    feat: windicss

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+const WindiCSSWebpackPlugin = require('windicss-webpack-plugin')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack(config) {
+    config.plugins.push(new WindiCSSWebpackPlugin())
+    return config
+  },
 }
 
 module.exports = nextConfig
