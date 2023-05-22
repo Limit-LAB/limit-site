a97a05b15f2a131b956eac704bba058385ad2eec
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2023-05-22 11:13:33 +0800

    chore: init brunch using `create-next-app`

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
-import WindiCSSWebpackPlugin from 'windicss-webpack-plugin'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  compress: true,
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  images: {
-    domains: ['placekitten.com'],
-    unoptimized: true
-  },
-  webpack(config) {
-    config.plugins.push(new WindiCSSWebpackPlugin())
-    return config
-  }
 }
 
-export default nextConfig
+module.exports = nextConfig
